6b51523be19f9aa75d0737a9d9e11432abb22986
SaMuRaIdevv <mukhridd1nchoriyev2007@gmail.com>
2024-03-23 15:39:20 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,227 @@
+"use strict"
+
+// IF-12
+
+// let a = +prompt('Birinchi sonni kiriting',"")
+// let b = +prompt('Ikkinchi sonni kiriting',"")
+// let c = +prompt('Uchinchi sonni kiriting',"")
+
+
+// if(a < b && a < c){
+//     alert(`${a} soni eng kichik`)
+// }else if(b < a && b < c){
+//     alert(`${b} soni eng kichik`)
+// }else if(c < a && c < b){
+//     alert(`${c} soni eng kichik`)
+// }else if(a === b && b === c){
+//     alert(`3ta son teng`)
+// }else{
+//     alert('Error!')
+// }  
+
+// IF-13
+
+// let a = +prompt('Birinchi sonni kiriting',"")
+// let b = +prompt('Ikkinchi sonni kiriting',"")
+// let c = +prompt('Uchinchi sonni kiriting',"")
+
+// if(a < b && a > c){
+//   alert(`${a}`)
+// }else if(c < b && b > a){
+//     alert(`${b}`)
+// }else if(a < c && c > b){
+//     alert(`${c}`)
+// }else{
+//     alert(`Error!`)
+// }
+
+// IF-14
+
+// let a = +prompt('Birinchi sonni kiriting',"")
+// let b = +prompt('Ikkinchi sonni kiriting',"")
+// let c = +prompt('Uchinchi sonni kiriting',"")
+
+
+// if(a < b && a < c && b < c){
+//     alert(`${a} soni eng kichik`);
+//     alert(`${c} soni eng katta`);
+// }else if(a < b && a < c && c < b){
+//     alert(`${a} soni eng kichik`);
+//     alert(`${b} soni eng katta`);
+// }else if(b < a && b < c && a < c){
+//     alert(`${b} soni eng kichik`);
+//     alert(`${c} soni eng katta`);
+// }else if(b < a && b < c && c < a){
+//     alert(`${b} soni eng kichik`);
+//     alert(`${a} soni eng katta`);
+// }else if(c < a && c < b && a < b){
+//     alert(`${c} soni eng kichik`);
+//     alert(`${a} soni eng katta`);
+// }else if(c < a && c < b && b < a){
+//     alert(`${c} soni eng kichik`)
+//     alert(`${b} soni eng katta`);
+// }else if(a === b && b === c){
+//     alert(`3ta son teng`)
+// }else{
+//     alert('Error!')
+// }  
+
+// IF-15
+
+// let a = 5
+// let b = 9
+// let c = 3
+
+// if (c > a && b > a) {
+//     console.log(c+b)
+// }else if(c > b && a > b){
+//     console.log(c+a)
+// }else if(a > c && b > c){
+//     console.log(a+b)
+// }
+
+
+// IF-16
+
+// let a = 2
+// let b = 4
+// let c = 9
+
+// if(a < b && b < c){
+//     console.log(2*a)
+//     console.log(2*b)
+//     console.log(2*c)
+// }else{
+//     console.log(-a)
+//     console.log(-b)
+//     console.log(-c)
+// } 
+
+
+// IF-17
+
+// let a = 13
+// let b = 10
+// let c = 9
+
+// if(a < b && b < c){
+//     console.log(2*a)
+//     console.log(2*b)
+//     console.log(2*c)
+// }else if(a > b && b > c){
+//     console.log(2*a)
+//     console.log(2*b)
+//     console.log(2*c)
+// }else{
+//     console.log(-a)
+//     console.log(-b)
+//     console.log(-c)
+// } 
+
+// IF-18
+
+// let a = 3
+// let b = 2
+// let c = 2
+
+// if(a === b){
+//     console.log(3)
+// }else if(a === c){
+//     console.log(2)
+// }else if(c === b){
+//     console.log(1)
+// }else{
+//     console.log(Error);
+// }
+
+// IF-19
+
+// let a = 2
+// let b = 2
+// let c = 2
+// let d = 3
+
+// if(a === b , b === c){
+//     console.log(4);
+// }else if(a === b , b === d){
+//     console.log(3);
+// }else if(a === d , d === c){
+//     console.log(2);
+// }else if(d === b , b === c){
+//     console.log(1);
+// }else{
+//     console.log(Error);
+// }
+
+// IF-20
+
+// let a = 5
+// let b = 11
+// let c = 9
+
+// if(a < b && b < c){
+//     console.log("b" , b - a);
+// }else if(a < c && b > c){
+//     console.log("c" , c - a);
+// }else{
+//     console.log(Error);
+// }
+
+// IF-21
+
+// let aX = 8
+// let aY = 3
+
+// if(aX === 0 && aY === 0){
+//     console.log(0);
+// }else if(aX !== 0 && aY === 0){
+//     console.log(1 , 2);
+// }else if(aY !== 0 && aX === 0){
+//     console.log(1 , 2);
+// }else{
+//     console.log(3);
+// }
+
+// IF-22
+
+// let aX = 8
+// let aY = -3
+
+// if(aX > 0 && aY > 0){
+//     console.log(1);
+// }else if(aX < 0 && aY > 0){
+//     console.log(2);
+// }else if(aX < 0 && aY < 0){
+//     console.log(3);
+// }else if(aX > 0 && aY < 0){
+//     console.log(4);
+// }else{
+//     console.log(Error);
+// }
+
+// IF-23
+
+// let aX1 = 3
+// let aY1 = 5
+// let aX2 = 3
+// let aY2 = 5
+// let aX3 = 8
+// let aY3 = 5
+// let aX4 = 8
+// let aY4 = 3
+
+// if(aX4 === aX3 && aY2 === aY3){
+//     console.log(aX2);
+//     console.log(aY4);
+// }else if(aX3 === aX4 && aY1 === aY4){
+//     console.log(aX1);
+//     console.log(aY3);
+// }else if(aX1 === aX2 && aY1 === aY4){
+//     console.log(aX4);
+//     console.log(aY2);
+// }else if(aX1 === aX2 && aY2 === aY3){
+//     console.log(aX3);
+//     console.log(aY1);
+// }else{
+//     console.log(Error);
+// }
